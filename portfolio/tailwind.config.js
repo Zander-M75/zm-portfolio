78ffae57a3ae2fa613c1633e4581a790f6c4344f
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-10-12 15:59:58 -0400

    added custom fonts and color palette

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        333: "#333333",
+        ECF0F1: "#ECF0F1",
+      },
+    },
   },
   plugins: [],
 };
